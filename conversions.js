6256aad02b1fd9f8922bfd261e6750ba3e24a7ca
Adrika <adrikad97@gmail.com>
2025-07-12 13:59:21 +0530

    conversion and operator

---
+++ conversions.js
@@ -0,0 +1,20 @@
+
+let str1= "Hello"
+let str2 = "adrika"
+
+let str3  = str1 + " " + str2
+
+// console.log(str3); // Output: Hello adrika
+
+// console.log(1+1+"2");        // Output: 22
+// console.log(1+"2"+3);        // Output: 123
+// console.log(1+1+"2");        // Output: 22
+// console.log("2" + 1 + 1);    // Output: 211
+
+
+console.log(+true);       // Output: 1
+console.log(+"");               // Output: 0
+
+let gameCounter = 100;
+console.log(++gameCounter); // Output: 101
+console.log(gameCounter++); // Output: 101
