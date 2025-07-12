03bb029839ca93710b7dada9d156f9a8a2948178
Adrika <adrikad97@gmail.com>
2025-07-12 13:42:54 +0530

    javascript practice #Initial commit

---
+++ basic01.js
@@ -0,0 +1,22 @@
+
+const name = "Adrika";
+let age = 23;
+var email = "adrikad97@gmail.com"
+city = "Varanasi"
+
+let lastName;
+
+console.table([name , age , email , city , lastName])
+
+// name = "Ayush" 
+/* 
+prefer not to use var in modern JavaScript
+as it has function scope and can lead to unexpected behavior.
+*/
+
+age = 25
+email = "akdrinni@gmail.com"
+city = "Bengaluru"
+lastName = "Dwivedi"
+
+console.table([name , age , email , city , lastName])
